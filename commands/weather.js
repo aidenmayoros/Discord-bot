e3958d605fa259a10b4b46bc9a54794b2ae2ce38
Aiden Mayoros <aiden.mayoros@gmail.com>
2022-10-13 20:04:57 -0700

    Weather bot is working

--- commands/weather.js
+++ commands/weather.js
@@ -1,40 +1,36 @@
 const { SlashCommandBuilder } = require('discord.js');
-const forecast = require('../weather app/forecast');
 const axios = require('axios');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('weather')
-		.setDescription('Replies with current weather data for given location')
-		.addStringOption((option) =>
-			option
-				.setName('location')
-				.setDescription('Location to get weather data')
-				.setRequired(true)
-		),
-	async execute(interaction) {
-		const weatherLocation = interaction.options.getString('location');
+  data: new SlashCommandBuilder()
+    .setName('weather')
+    .setDescription('Replies with current weather data for given location')
+    .addStringOption((option) =>
+      option
+        .setName('location')
+        .setDescription('Location to get weather data')
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const weatherLocation = interaction.options.getString('location');
 
-		//this is not working
-		//use try catch
-		//use await on api calls
+    // This means tell Discord that this might possibly take 3+ seconds
+    interaction.deferReply({ ephemeral: true });
 
-		await axios
-			.get(
-				'https://api.myptv.com/geocoding/v1/locations/by-text?searchText=' +
-					weatherLocation +
-					'&apiKey=N2JiNzA0OWM4MzZiNGVmODgzNWI3ZGEzNGQ5Njk4ZDI6YmFlMTkyODYtOTYwMS00MGNjLWFhODEtNTkzNzVmZGU4MGZl'
-			)
-			.then((res) => {
-				console.log(res.data.locations[0].referencePosition);
-			})
-			.catch((error) => {
-				console.log(error);
-			});
+    const GEOCODE_LOCATION_URL = `https://api.myptv.com/geocoding/v1/locations/by-text?searchText=${weatherLocation}&apiKey=N2JiNzA0OWM4MzZiNGVmODgzNWI3ZGEzNGQ5Njk4ZDI6YmFlMTkyODYtOTYwMS00MGNjLWFhODEtNTkzNzVmZGU4MGZl`;
 
-		await interaction.reply({
-			content: `Weather response for ${weatherLocation}!`,
-			ephemeral: true,
-		});
-	},
+    const geocodeData = await axios.get(GEOCODE_LOCATION_URL);
+
+    const { latitude, longitude } =
+      geocodeData.data.locations[0].referencePosition;
+
+    const WEATHER_URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&exclude=hourly,minutely,alerts&appid=88b895fa4815bc85c2b6ee08540fbf86`;
+
+    const weatherData = await axios.get(WEATHER_URL);
+
+    await interaction.editReply({
+      content: `The temperature of ${weatherLocation} is ${weatherData.data.current.temp.toString()}!`,
+      ephemeral: true
+    });
+  }
 };
